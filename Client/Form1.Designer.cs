8aa6dbd3ea13a20f22124ef2d7e36541d6497da5
Przemek <Darkprzemq@gmail.com>
2018-03-03 18:38:34 +0100

    Initial commit

---
+++ Client/Form1.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace Client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Login = new System.Windows.Forms.Label();
+            this.Password = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.ProductList = new System.Windows.Forms.ComboBox();
+            this.Dodaj = new System.Windows.Forms.Button();
+            this.OrderBox = new System.Windows.Forms.ListBox();
+            this.Delete = new System.Windows.Forms.Button();
+            this.SendOrder = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(161, 43);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "połącz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Login
+            // 
+            this.Login.AutoSize = true;
+            this.Login.Location = new System.Drawing.Point(13, 13);
+            this.Login.Name = "Login";
+            this.Login.Size = new System.Drawing.Size(33, 13);
+            this.Login.TabIndex = 1;
+            this.Login.Text = "Login";
+            // 
+            // Password
+            // 
+            this.Password.AutoSize = true;
+            this.Password.Location = new System.Drawing.Point(13, 48);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(36, 13);
+            this.Password.TabIndex = 2;
+            this.Password.Text = "Hasło";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(55, 13);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(55, 45);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            // 
+            // ProductList
+            // 
+            this.ProductList.FormattingEnabled = true;
+            this.ProductList.Location = new System.Drawing.Point(16, 114);
+            this.ProductList.Name = "ProductList";
+            this.ProductList.Size = new System.Drawing.Size(214, 21);
+            this.ProductList.TabIndex = 5;
+            // 
+            // Dodaj
+            // 
+            this.Dodaj.Location = new System.Drawing.Point(236, 112);
+            this.Dodaj.Name = "Dodaj";
+            this.Dodaj.Size = new System.Drawing.Size(75, 23);
+            this.Dodaj.TabIndex = 9;
+            this.Dodaj.Text = "Dodaj>>";
+            this.Dodaj.UseVisualStyleBackColor = true;
+            this.Dodaj.Click += new System.EventHandler(this.Dodaj_Click);
+            // 
+            // OrderBox
+            // 
+            this.OrderBox.FormattingEnabled = true;
+            this.OrderBox.Location = new System.Drawing.Point(317, 114);
+            this.OrderBox.Name = "OrderBox";
+            this.OrderBox.Size = new System.Drawing.Size(215, 277);
+            this.OrderBox.TabIndex = 10;
+            // 
+            // Delete
+            // 
+            this.Delete.Location = new System.Drawing.Point(236, 141);
+            this.Delete.Name = "Delete";
+            this.Delete.Size = new System.Drawing.Size(75, 23);
+            this.Delete.TabIndex = 11;
+            this.Delete.Text = "<<Usuń";
+            this.Delete.UseVisualStyleBackColor = true;
+            this.Delete.Click += new System.EventHandler(this.Delete_Click);
+            // 
+            // SendOrder
+            // 
+            this.SendOrder.Location = new System.Drawing.Point(236, 358);
+            this.SendOrder.Name = "SendOrder";
+            this.SendOrder.Size = new System.Drawing.Size(75, 23);
+            this.SendOrder.TabIndex = 12;
+            this.SendOrder.Text = "Wyślij->";
+            this.SendOrder.UseVisualStyleBackColor = true;
+            this.SendOrder.Click += new System.EventHandler(this.SendOrder_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(544, 393);
+            this.Controls.Add(this.SendOrder);
+            this.Controls.Add(this.Delete);
+            this.Controls.Add(this.OrderBox);
+            this.Controls.Add(this.Dodaj);
+            this.Controls.Add(this.ProductList);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.Login);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Login;
+        private System.Windows.Forms.Label Password;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.ComboBox ProductList;
+        private System.Windows.Forms.Button Dodaj;
+        private System.Windows.Forms.ListBox OrderBox;
+        private System.Windows.Forms.Button Delete;
+        private System.Windows.Forms.Button SendOrder;
+    }
+}
+
---
+++ Client/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;//watek
+using System.Data.OleDb;//access
+
+
+namespace Client
+{
+    public partial class Form1 : Form
+    {
+        List<Skladnik> l_Skladnik = new List<Skladnik>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamReader sr;
+            ASCIIEncoding asen = new ASCIIEncoding();
+            TcpClient tcpclnt = new TcpClient();
+            tcpclnt.Connect("127.0.0.1", 8001);
+            Stream stm = tcpclnt.GetStream();
+            string str;
+            byte[] ba;
+            byte[] bb;
+            string tekst;
+            int k;
+            do
+            {
+                str = "L";//Przesłanie komunikatu o checi zalogowania
+                ba = asen.GetBytes(str);
+                stm.Write(ba, 0, ba.Length);
+                bb = new byte[100];
+                k = stm.Read(bb, 0, 100);
+                tekst = "";
+                for (int i = 0; i < k; i++) tekst += (Convert.ToChar(bb[i]));
+            } while (tekst != "OK");//potwierdzenie od serwera o przyjeciu checi logowania
+            
+
+            str = textBox1.Text + " " + textBox2.Text;//login i hasło
+            ba = asen.GetBytes(str);
+            stm.Write(ba, 0, ba.Length);
+            bb = new byte[100];
+            k = stm.Read(bb, 0, 100);
+            tekst = "";
+            for (int i = 0; i < k; i++)tekst+= (Convert.ToChar(bb[i]));
+            if (tekst == "C")
+            {
+                using (var output = File.Create(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\WokItEasy1.txt")))
+                {
+                    var buffer = new byte[1024];
+                    int bytesRead;
+                    while ((bytesRead = stm.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        output.Write(buffer, 0, bytesRead);
+                    }
+                }
+                sr = new StreamReader(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"..\..\WokItEasy1.txt"));
+                string text = sr.ReadLine();
+                int ilosc = Convert.ToInt32(text);
+                for (int i = 0; i < ilosc; i++)
+                {
+                    text = sr.ReadLine();
+                    string[] splited = text.Split(' ');
+                    Skladnik skladnik = new Skladnik();
+                    skladnik.IdSM = Convert.ToInt32(splited[0]);
+                    skladnik.NazwaSM = splited[1];
+                    skladnik.RodzajSM = splited[2];
+                    skladnik.CenaSM = Convert.ToDouble(splited[3]);
+                    l_Skladnik.Add(skladnik);
+                }
+                foreach (var skladnik in l_Skladnik)
+                {
+                    tekst = skladnik.NazwaSM + "\t Cena: " + skladnik.CenaSM;
+                    ProductList.Items.Add(tekst);
+                }
+                ////MessageBox.Show("Logowanie zakonczone sukcesem."); // ten wycinek kodu jest starym kodem który nie będzie juz raczej używany
+                //k = stm.Read(bb, 0, 100);//wczytywanie ilosci elementów// ale może przydać sie mi do analizy. Usunę go później
+                //tekst = "";
+                //for (int i = 0; i < k; i++) tekst += (Convert.ToChar(bb[i]));
+                //int ilosc = Convert.ToInt32(tekst);
+                //ba = asen.GetBytes("OK");
+                //stm.Write(ba, 0, ba.Length);
+                //for(int i=0;i<ilosc;i++)
+                //{
+                //    //MessageBox.Show("Klient Stop1");
+                //    bb = new byte[100];
+                //    k = stm.Read(bb, 0, 100);
+                //    tekst = "";
+                //    for (int j = 0; j < k; j++) tekst += (Convert.ToChar(bb[j]));
+                //    //MessageBox.Show(tekst);
+                //    ba = asen.GetBytes("OK");
+                //    stm.Write(ba, 0, ba.Length); //odbiór i potwierdzenie odbioru danych
+                //    // dodanie danych do klasy
+                //}
+
+
+
+
+            } 
+            else if (tekst == "W") MessageBox.Show("Niepoprawne dane");
+            tcpclnt.Close();
+            //catch (Exception e)
+            //{
+            //    Console.WriteLine("Error..... " + e.StackTrace);
+            //}
+        }
+
+        private void Dodaj_Click(object sender, EventArgs e)
+        {
+            OrderBox.Items.Add(ProductList.Text);
+            // comboBox1.SelectedIndex
+            //string tekst = ProductList.Name;
+            //string[] split = tekst.Split('\t');
+            //foreach(var skladnik in l_Skladnik)
+            //{
+            //    if (skladnik.NazwaSM == split[0]) ;
+            //}
+        }
+
+        private void Delete_Click(object sender, EventArgs e)
+        {
+            //OrderBox.Items.Remove(OrderBox.SelectedIndex);
+            if (this.OrderBox.SelectedIndex >= 0)
+                this.OrderBox.Items.RemoveAt(this.OrderBox.SelectedIndex);
+        }
+
+        private void SendOrder_Click(object sender, EventArgs e)
+        {
+            ASCIIEncoding asen = new ASCIIEncoding();
+            TcpClient tcpclnt = new TcpClient();
+            tcpclnt.Connect("127.0.0.1", 8001);
+            Stream stm = tcpclnt.GetStream();
+            string str;
+            byte[] ba;
+            byte[] bb;
+            string tekst;
+            int k;
+            do
+            {
+                str = "O";//Przesłanie komunikatu o checi przeslania zamowienia
+                ba = asen.GetBytes(str);
+                stm.Write(ba, 0, ba.Length);
+                bb = new byte[100];
+                k = stm.Read(bb, 0, 100);
+                tekst = "";
+                for (int i = 0; i < k; i++) tekst += (Convert.ToChar(bb[i]));
+            } while (tekst != "OK");//potwierdzenie od serwera o przyjeciu checi logowania
+            do
+            {
+                str = Convert.ToString(OrderBox.Items.Count);//wysłanie komunikatu o ilosci elementów w zamówieniu
+                ba = asen.GetBytes(str);
+                stm.Write(ba, 0, ba.Length);
+                bb = new byte[100];
+                k = stm.Read(bb, 0, 100);
+                tekst = "";
+                for (int i = 0; i < k; i++) tekst += (Convert.ToChar(bb[i]));
+            } while (tekst != "OK");//potwierdzenie od serwera o przyjeciu ilosci elementów
+            string text="";
+            for (int i = 0; i <(OrderBox.Items.Count);i++)// przesył zamówień
+            {
+                text = OrderBox.Items[i].ToString();
+                string[] split = text.Split('\t');
+                //MessageBox.Show(split[0]);
+                int id = -1;
+                foreach(var skladnik in l_Skladnik)//ustalanie id produktu
+                {
+                    if (skladnik.NazwaSM == split[0] && id < 0)
+                    {
+                        id = skladnik.IdSM;
+                        //MessageBox.Show(Convert.ToString(id));
+                        break;
+                    }
+                    
+                    
+                    
+                }
+                
+                do//przesył id produktu
+                {
+                    str = Convert.ToString(id);
+                    ba = asen.GetBytes(str);
+                    stm.Write(ba, 0, ba.Length);
+                    bb = new byte[100];
+                    k = stm.Read(bb, 0, 100);
+                    tekst = "";
+                    for (int j = 0; j < k; j++) tekst += (Convert.ToChar(bb[j]));
+                } while (tekst != "OK");
+                
+            }
+            tcpclnt.Close();
+        }
+    }
+}
